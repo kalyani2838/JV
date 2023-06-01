f666d95b0cf0233a348f3d460b3a880fcafaae50
kalyani2838 <135198548+kalyani2838@users.noreply.github.com>
2023-06-01 20:00:15 +0530

    Created using Colaboratory

---
+++ AddFloatingNumbers.java
@@ -0,0 +1,88 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNu/rjRg/qLr6L6jePEU361",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/kalyani2838/JV/blob/main/AddFloatingNumbers.java\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "ginsp7ZFXqSb"
+      },
+      "outputs": [],
+      "source": []
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 130
+        },
+        "id": "1RT-VRcjX5Nq",
+        "outputId": "56f350ee-8ee7-477d-ed20-5ec94ea6856f"
+      },
+      "execution_count": 5,
+      "outputs": [
+        {
+          "output_type": "error",
+          "ename": "SyntaxError",
+          "evalue": "ignored",
+          "traceback": [
+            "\u001b[0;36m  File \u001b[0;32m\"<ipython-input-5-d9a35d832039>\"\u001b[0;36m, line \u001b[0;32m1\u001b[0m\n\u001b[0;31m    public class AddFloatingNumbers{\u001b[0m\n\u001b[0m           ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m invalid syntax\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "gZIwFZiqWECl"
+      },
+      "execution_count": 5,
+      "outputs": []
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "public class AddFloatingNumbers{\n",
+        "    \n",
+        "    public static void main (String args[]) {\n",
+        "        float i = 6.7;\n",
+        "        float j = 8.9;\n",
+        "        float sum = i + j;\n",
+        "        System.out.println(\"sum of two floating numbers: \" + sum);\n",
+        "    }\n",
+        "}"
+      ],
+      "metadata": {
+        "id": "PRDFqNo8WFJM"
+      }
+    }
+  ]
+}
